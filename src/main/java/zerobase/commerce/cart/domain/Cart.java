c23f62c40f780ce0d2a72fa31b58085175c551e0
woojin <tkdalsrb147@naver.com>
2025-02-14 23:33:09 +0900

    refactor: Cart에 EntityListeners 어노테이션 추가
    - createdAt과 updatedAt을 활성화 하기 위해 어노테이션 추가

--- src/main/java/zerobase/commerce/cart/domain/Cart.java
+++ src/main/java/zerobase/commerce/cart/domain/Cart.java
@@ -2,11 +2,13 @@ package zerobase.commerce.cart.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +17,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import zerobase.commerce.product.domain.Product;
 import zerobase.commerce.user.domain.User;
 
@@ -24,6 +27,8 @@ import zerobase.commerce.user.domain.User;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "cart")
+@EntityListeners(AuditingEntityListener.class)
 public class Cart {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
